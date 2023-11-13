f6f07d307d4cdcb6b5d24072615124f060ad1a2d
luccas03x <136741892+luccas03x@users.noreply.github.com>
2023-11-13 16:37:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,158 @@
+<html>
+    <head>
+        <title>TV Clic Brasil © | 2020</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    </head>
+
+    <style>
+        body, html {
+            height: 100%;
+            font-family: "Inconsolata", sans-serif;
+        }
+
+        .bgimg {
+            background-position: center;
+            background-size: cover;
+            background-image: url("/w3images/coffeehouse.jpg");
+            min-height: 75%;
+        }
+
+        .menu {
+            display: none;
+        }
+    </style>
+
+    <body id="home">
+
+        <!-- Navbar -->
+        <div class="w3-top">
+            <div class="w3-row w3-padding w3-black">
+                <div class="w3-col s3">
+                    <a href="#home" class="w3-button w3-block w3-black">HOME</a>
+                </div>
+                <div class="w3-col s3">
+                    <a href="#quemsomos" class="w3-button w3-block w3-black">QUEM SOMOS?</a>
+                </div>
+                <div class="w3-col s3">
+                    <a href="#programacao" class="w3-button w3-block w3-black">PROGRAMAÇÃO</a>
+                </div>
+                <div class="w3-col s3">
+                    <a href="#contato" class="w3-button w3-block w3-black">CONTATO</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <img src="img/Banner Tv Clic .jpg" width="2000" height="750">
+
+        </div>
+
+        <div class="w3-sand w3-grayscale w3-large">
+
+            <!-- Quem somos? -->
+            <div class="w3-container" id="quemsomos">
+                <div class="w3-content" style="max-width:700px">
+                    <h3 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">QUEM SOMOS?</span></h3>
+                    <h5><b>Há 15 anos no mercado</b></h5>
+                    <p align="justify">A Tv Clic Brasilé uma empresa especializada em produção de conteúdo áudio-visual, incluindo programas jornalísticos e de entretenimento; reportagens e documentários destinados aos segmentos de Tv Convencional; IPTV; WebTv (televisão via internet) e Redes Sociais via Rede Mundial de Computadores.
+                    <p align="justify">A Tv Clic Brasil também faz parte da Rede Clic Brasil de Rádio, Cinema e Televisão, que se dedica a produção de conteúdos para estes segmentos.</p>
+                    <p align="justify">A programação da Tv Clic Brasil é veiculada diariamente, ao vivo, via streaming e pode ser acessada via Smartv; Celular e Computador pelo site tvclicbrasil.com e também pelo seu Aplicativo (em sistema Android e IOS), e ainda pelo CANAL 12 da VIATVNET (sistema gratuito de canais). A Tv Clic Brasil também mantém páginas nas Redes Sociais: Facebook e Youtube.</p>
+
+                    <h5><b>Transmissões ao vivo e TV Câmera</b></h5>
+                    <p align="justify">A Tv Clic Brasil é especializada em transmissões ‘ao vivo’ personalizadas, de sessões legislativas (somos criadores de Tvs Câmaras Municipais) e eventos em geral, tanto pela internet, como Tv convencional, e de produção em vídeo de conteúdo para tais finalidades.</p>
+                    <p align="justify">A Tv Clic Brasil também é pioneira em criação, produção e manutenção de Tv Personalizada (Circuito Interno de Tv), além de produção de seu conteúdo e veiculação, serviços disponibilizados tanto para órgãos e empresas públicas, como empresas privadas.</p>
+
+                    <h5><b>Assessoria de imprensa</b></h5>
+                    <p align="justify">A Tv Clic Brasil oferece também pacote de serviços, que inclui: criação; personalização de WebTv e Tv Convencional; produção de conteúdo e manutenção 24 horas, além de serviços de apresentação e divulgação em todos os veículos de mídia (assessoria de imprensa) sobre as atividades do cliente, incluindo as grandes redes sociais.</p>
+
+                    <h5><b>Produções cinematográficas</b></h5>
+                    <p align="justify">A Tv Clic Brasil também é atuante no mercado cinematográfico, desenvolvendo e produzindo filmes de curta e longa metragens, além de séries e novelas, e uma diversidade de produtos finalizados com formatos para reprodução em salas de cinema, televisão e webtv.</p>
+
+                    <h5><b>Volume de Visitação Ilimitado</b></h5>
+                    <p align="justify">A programação (ao vivo e gravada) da TvClicBrasil.com é composta de entrevistas; programas de entretenimento; musicais; telejornais; reportagens; filmes; documentários e transmissão de eventos diversos e interativos.</p>
+                    <p align="justify">Além disso, essa grade é transmitida (além do CANAL 12 da VIATVNET) também pelos canais exclusivos da Tv Clic Brasil no YouTube e Facebook. Utilizando os links da Tv Clic Brasil, o cliente também pode difundir seu produto e, ou, serviço, conforme interesse, postando-os em sites de relacionamentos e nas páginas de amigos, que serão visualizados por todos os navegadores. Isso torna a programação da Tv Clic Brasil e dos seus anúncios, com volume de visitações ilimitado.</p>
+
+                    <h5><b>Diferenciais e Comprometimento</b></h5>
+                    <p align="justify">Tanto a criação, quanto a manutenção e disponibilização dos serviços da Tv Clic Brasil são avalizados pela experiência de seu corpo profissional, e diferenciais de trabalho, que destacam a funcionalidade 24 horas e o comprometimento com a qualidade no desenvolvimento de projetos objetivos e eficazes.</p>
+
+                </div>
+            </div>
+
+            <!-- Programação -->
+            <div class="w3-container" id="programacao">
+                <div class="w3-content" style="max-width:700px">
+                    <h3 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">PROGRAMAÇÃO</span></h3>
+
+                    <div class="w3-panel w3-leftbar w3-light-grey">
+                        <p>CLIC MISTÉRIOS</p>
+                        <p>CLIC SP</p>
+                        <p>CULTURA</p>
+                        <p>EM CASA COM SANDRA LIMA</p>
+                        <p>ENTREVISTA</p>
+                        <p>ESPORTE</p>
+                        <p>HUMOR</p>
+                        <p>INFORMATIVO</p>
+                        <p>NOTÍCIAS</p>
+                        <p>PROGRAMA BALADA COUNTRY BRASIL</p>
+                        <p>PROGRAMA CLÁUDIO DINIZ</p>
+                        <p>PROGRAMA CONTEXTO</p>
+                        <p>PROGRAMA GLÓRIA FRITOLI</p>
+                        <p>PROGRAMA PROPOSTA DE VALOR</p>
+                        <p>PROGRAMA RANCHO DO JECA</p>
+                        <p>PROGRAMA SUELI GAZOLLA</p>
+                        <p>PROGRAMA VITÓRIA DE DEUS</p>
+                        <p>SOS SAÚDE</p>
+                        <p>VOCÊ MERECE SER LIVRE</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Contato -->
+            <center>
+                <div class="w3-container w3-padding-64" id="contato">
+                    <div class="w3-row">
+                        <h3 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">CONTATO</span></h3>
+                        <p><i class="fa fa-map-marker w3-text-blue w3-xlarge"></i>  Rua Machado de Assis, 797 – Sala 813 – Centro – Osasco</p>
+                        <p><i class="fa fa-envelope-o w3-text-blue w3-xlarge"></i>  amauryoliveiratvclicbrasil@gmail.com</p>
+                        <p><i class="fa fa-instagram w3-text-blue w3-xlarge"></i>  @tvclicbrasiloficial</p>
+                        <p><i class="fa fa-phone w3-text-blue w3-xlarge"></i>  +11 98599-0357</p>
+                        <p><i class="fa fa-phone w3-text-blue w3-xlarge"></i>  +11 3682-0506</p>
+                    </div>
+
+                </div>
+            </center>
+
+        </div>
+
+        <!-- Rodapé -->
+        <footer class="w3-center w3-light-grey w3-padding-48 w3-large">
+            <p>© 2020 | TV Clic Brasil | Desenvolvido por Alloy</p>
+        </footer>
+
+        <script>
+            // Tabbed Menu
+            function openMenu(evt, menuName) {
+                var i, x, tablinks;
+                x = document.getElementsByClassName("menu");
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                tablinks = document.getElementsByClassName("tablink");
+                for (i = 0; i < x.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+                }
+                document.getElementById(menuName).style.display = "block";
+                evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+            }
+            document.getElementById("myLink").click();
+        </script>
+
+    </body>
+</html>
